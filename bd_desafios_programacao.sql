6686d85f1dce52db54df2dcc6c8e8e7a89d1aa1f
Bruno <git@github.com:bruninho51/Menu-dropdown-css.git>
2018-03-18 18:45:56 -0300

    modificações no banco de dados

--- bd_desafios_programacao.sql
+++ bd_desafios_programacao.sql
@@ -1,16 +1,20 @@
+DROP DATABASE bd_desafio_programacao;
 CREATE DATABASE bd_desafio_programacao;
 USE bd_desafio_programacao;
-CREATE TABLE desafio(
-	id INT AUTO_INCREMENT,
-    enunciado TEXT,
-    realizado BOOLEAN,
-    dificuldade CHARACTER,
-    PRIMARY KEY(id)
-);
-
 CREATE TABLE programador(
 	id INT AUTO_INCREMENT,
     nickname VARCHAR(15),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE desafio(
+	id INT AUTO_INCREMENT,
+    id_programador INT,
+    enunciado TEXT,
+    resposta TEXT,
+    realizado BOOLEAN,
+    dificuldade CHARACTER,
+    CONSTRAINT fk_programador FOREIGN KEY(id_programador) REFERENCES programador(id),
     PRIMARY KEY(id)
 );
 
@@ -18,8 +22,10 @@ CREATE TABLE desafios_programadores(
 	id INT AUTO_INCREMENT,
     id_desafio INT,
     id_programador INT,
-    link_codigo_github TEXT,
-    CONSTRAINT fk_desafio FOREIGN KEY(id_desafio) REFERENCES desafio(id),
-    CONSTRAINT fk_programador FOREIGN KEY(id_programador) REFERENCES programador(id),
+    correta BOOLEAN,
+    link_arquivo TEXT,
+    CONSTRAINT cons_desafio FOREIGN KEY(id_desafio) REFERENCES desafio(id),
+    CONSTRAINT cons_programador FOREIGN KEY(id_programador) REFERENCES programador(id),
     PRIMARY KEY(id)
 );
+
